041c8a5d862101ee493fe6d42bd7e9aef6378517
fcu-d0409874 <jim23693728@gmail.com>
2016-10-14 23:09:20 +0800

    hw3

---
+++ src/FourInOne/Break.java
@@ -0,0 +1,14 @@
+package FourInOne;
+
+public class Break {
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			if (i > 1) {
+				break;
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
--- src/FourInOne/StringComparator.java
+++ src/FourInOne/StringComparator.java
@@ -5,16 +5,19 @@ import java.util.Scanner;
 public class StringComparator {
 	public static void main(String[] args) {
 		Scanner Keyborad = new Scanner(System.in);
-		System.out.printf("Enter a string 1: ");
-		String str1 = Keyborad.next();
-		System.out.printf("Enter a string 2: ");
-		String str2 = Keyborad.next();
-		if (str1.compareToIgnoreCase(str2) == 0) {
-			System.out.printf("The two strings are the same.");
-		} else {
-			System.out.printf("The two strings are not the same.");
-		}
-
+		while (true){
+			System.out.printf("Enter a string 1: ");
+			String str1 = Keyborad.next();
+			System.out.printf("Enter a string 2: ");
+			String str2 = Keyborad.next();
+			if (str1.compareToIgnoreCase(str2) == 0) {
+				System.out.printf("The two strings are the same.");
+				break;
+			} else {
+				System.out.printf("The two strings are not the same.\n");
+				continue;
+			}
+		} ;
 		Keyborad.close();
 	}
 
